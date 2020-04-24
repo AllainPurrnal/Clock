ebd7c7ea5cc023ebe6d9b465649a41f238b83f9f
AllainPurrnal <llnbrnl017@gmail.com>
2020-04-24 16:53:49 -0700

    MVP up and running, want to add other features. Digital Clock, stop watch, alarm add/del/edit

---
+++ index.js
@@ -0,0 +1,25 @@
+const hourHand = document.querySelector('#hour_hand');
+const minuteHand = document.querySelector('#min_hand');
+const secondHand = document.querySelector('#sec_hand');
+
+function setDate() {
+  const now = new Date();
+
+  const hours = now.getHours();
+  const hoursDegrees = ((hours / 60) * 360) + 90;
+
+  const minutes = now.getMinutes();
+  const minutesDegrees = ((minutes / 60) * 360) + 90;
+
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  
+  // console.log(hours, minutes, seconds);
+  // console.log(now.getUTCDate())
+}
+
+setInterval(setDate, 1000);
