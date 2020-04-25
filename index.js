980ed2603ecfe0effa1daf2b01225a3172b18b10
AllainPurrnal <llnbrnl017@gmail.com>
2020-04-24 22:37:53 -0700

    jk, did not fix the glitch. toseed it for now

--- index.js
+++ index.js
@@ -14,30 +14,13 @@ const setDate = () => {
   const seconds = now.getSeconds();
   const secondsDegree = ((seconds / 60) * 360) + 90;
 
-  const jumpFix = (hand, deg) => {
-    // console.log(hand, deg);
-
-    if (deg >= 440) {
-      hand.style.transition = `none`;
-
-      if (deg >= 0) {
-        hand.style.transition = `all 0.05s`;
-        hand.style.transitionTimingFunction = `cubic-bezier(0.1, 2.7, 0.58, 1)`;
-      }
-    }
+  const moveHand = (hand, deg) => {
+    hand.style.transform = `rotate(${deg}deg)`;
   }
 
-  jumpFix(hourHand, hoursDegree)
-  hourHand.style.transform = `rotate(${hoursDegree}deg)`;
-
-  jumpFix(minuteHand, minutesDegree);
-  minuteHand.style.transform = `rotate(${minutesDegree}deg)`;
-
-  jumpFix(secondHand, secondsDegree);
-  secondHand.style.transform = `rotate(${secondsDegree}deg)`;
-  
-  // console.log(hours, minutes, seconds);
-  // console.log(now.getUTCDate())
+  moveHand(hourHand, hoursDegree);
+  moveHand(minuteHand, minutesDegree);
+  moveHand(secondHand, secondsDegree);
 }
 
 setInterval(setDate, 1000);
