9bd41017e19a154c90e19b9d1960bfa27142036c
AllainPurrnal <llnbrnl017@gmail.com>
2020-04-24 22:00:56 -0700

    Fixed glitch where hands visibly reverse to get back to starting position

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
-const hourHand = document.querySelector('#hour_hand');
-const minuteHand = document.querySelector('#min_hand');
-const secondHand = document.querySelector('#sec_hand');
+const hourHand = document.querySelector('.hour-hand');
+const minuteHand = document.querySelector('.minute-hand');
+const secondHand = document.querySelector('.second-hand');
 
 const setDate = () => {
   const now = new Date();
 
   const hours = now.getHours();
-  const hoursDegrees = ((hours / 60) * 360) + 90;
+  const hoursDegree = ((hours / 12) * 360) + 90;
 
   const minutes = now.getMinutes();
-  const minutesDegrees = ((minutes / 60) * 360) + 90;
+  const minutesDegree = ((minutes / 60) * 360) + 90;
 
   const seconds = now.getSeconds();
-  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  const secondsDegree = ((seconds / 60) * 360) + 90;
 
-  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
-  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
-  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  const jumpFix = (hand, deg) => {
+    // console.log(hand, deg);
+
+    if (deg >= 440) {
+      hand.style.transition = `none`;
+
+      if (deg >= 0) {
+        hand.style.transition = `all 0.05s`;
+        hand.style.transitionTimingFunction = `cubic-bezier(0.1, 2.7, 0.58, 1)`;
+      }
+    }
+  }
+
+  jumpFix(hourHand, hoursDegree)
+  hourHand.style.transform = `rotate(${hoursDegree}deg)`;
+
+  jumpFix(minuteHand, minutesDegree);
+  minuteHand.style.transform = `rotate(${minutesDegree}deg)`;
+
+  jumpFix(secondHand, secondsDegree);
+  secondHand.style.transform = `rotate(${secondsDegree}deg)`;
   
   // console.log(hours, minutes, seconds);
   // console.log(now.getUTCDate())
